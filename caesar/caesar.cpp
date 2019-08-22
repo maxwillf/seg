b4efba19ff7d090ffe4ef0adb6b7ac8eade54695
Max William Souto Filgueira <maxsnv@A103-19L.imd.local>
2019-08-22 20:01:15 -0300

    done

--- caesar/caesar.cpp
+++ caesar/caesar.cpp
@@ -6,19 +6,25 @@ int main(int argc, char * argv[]){
 	
 	std::string alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ?,";
 	unsigned char character;	
-	unsigned int chave = 1;
+	int chave = 0;
 	std::string outputPath = argv[2];
-	std::string originalOutPath = outputPath;
-	outputPath += '1';
 	std::ifstream input(argv[1]);
 	std::ofstream output(outputPath);
-	
 	while(chave < alfabeto.size()) {	
+		output << "Chave " << chave << std::endl;
 		while(input >> character){
 			int index = alfabeto.find(character);
 			if(index != std::string::npos){
-				int newIndex = (index + chave) % alfabeto.size();
-				output << alfabeto[newIndex];				
+				int newIndex = index - chave;
+
+				if(newIndex < 0) {
+					newIndex = alfabeto.size() + newIndex;
+				}
+				else {
+					newIndex = newIndex % alfabeto.size();
+				}
+
+				output << alfabeto[newIndex];	
 			}
 			else {
 				output << character;				
@@ -26,12 +32,11 @@ int main(int argc, char * argv[]){
 		}
 		chave++;
 		input = std::ifstream(argv[1]);
-		outputPath = (originalOutPath + std::to_string(chave));
-		output = std::ofstream(outputPath);
+		output << std::endl << std::endl;
 	}
 
 	// dê 
 	// compilar com g++ -std=c++11 caesar.cpp -o caesar
 	// rodar com ./caesar input.txt out
-	// cat out22 no shell e lá estará a mensagem decodificada
+	// cat out33 no shell e lá estará a mensagem decodificada
 }
