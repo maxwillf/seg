b87ac6d49c6ba0044ef2aaac8e909596a88e96f1
Max William Souto Filgueira <maxsnv@A103-32L.imd.local>
2019-08-27 20:36:32 -0300

    TabError

--- Steganography/Q3.py
+++ Steganography/Q3.py
@@ -1,83 +1,85 @@
-import cv2,sys
+import cv2
+import sys
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser()
-
-#-db DATABASE -u USERNAME -p PASSWORD -size 20000
-parser.add_argument("-host", "--hostname", dest = "hostname", default = "xyz.edu", help="Server name")
-
-def changeLastBit(byte,bitChar):
+def changeLastBit(byte, bitChar):
     if bitChar == '0':
         return byte & 0b11111110
     else:
         return byte | 0b00000001
+
+
 def getLastBit(byte):
     return '{0:08b}'.format(byte)[7]
+
+
 def getLetterAsByte(char):
     return '{0:08b}'.format(ord(char))
 
 
-src = sys.argv[1]
-message = sys.argv[2]
-message += '\x03'
+def cipher():
+	src = sys.argv[1]
+	message = sys.argv[2]
+	message += '\x03'
 
-print(getLetterAsByte(message[-1]))
+	print(getLetterAsByte(message[-1]))
 
-bitsNeededForMessage = len(message) * 8
-image = cv2.imread(src)
-imageSize = np.prod(image.shape)
+	bitsNeededForMessage = len(message) * 8
+	image = cv2.imread(src)
+	imageSize = np.prod(image.shape)
 
-old_shape = image.shape
+	old_shape = image.shape
 
-image = image.reshape(np.prod(image.shape))
-#print(image[:5])
-line = image[0]
+	image = image.reshape(np.prod(image.shape))
 
-lineIndex = 0
-columnIndex = 0
-pixelIndex = 0
+	pixelIndex = 0
 
-amountOfLines = old_shape[0]
-sizeOfLine = old_shape[1]
+	amountOfLines = old_shape[0]
+	sizeOfLine = old_shape[1]
 
-for letters in message:
-    for bit in getLetterAsByte(letters)[::-1]:
-        image[pixelIndex] = changeLastBit(image[pixelIndex],bit)
+	for letters in message:
+	    for bit in getLetterAsByte(letters)[::-1]:
+		image[pixelIndex] = changeLastBit(image[pixelIndex],bit)
+		pixelIndex += 1
+	    pixelIndex += 1
 
-        pixelIndex += 1;
-    pixelIndex += 1;
-    
 
-status = cv2.imwrite("./out.bmp",image.reshape(old_shape))
-print("Succesfully hidden message: ", status)
+	status = cv2.imwrite("./out.bmp", image.reshape(old_shape))
+	print("Succesfully hidden message: ", status)
 
-image = cv2.imread("./out.bmp")
 
-lineIndex = 0
-columnIndex = 0
-pixelIndex = 0
+def decipher():
+	image = cv2.imread("./out.bmp")
 
-image = image.reshape(np.prod(image.shape))
+	pixelIndex = 0
 
-decodedWord = ""
-readByte = ""
+	image = image.reshape(np.prod(image.shape))
 
-for letters in message:
-    for bit in getLetterAsByte(letters):
-        
-        readByte = getLastBit(image[pixelIndex]) + readByte
-        
-        if bit == 3:
-            print("Message found in file:",decodedWord)
-            break
-            
-        readByte = getLastBit(image[pixelIndex]) + readByte
+	decodedWord = ""
+	readByte = ""
 
-        if(decodedWord == message):
-            print("Message found in file:",decodedWord)
-            break
-        pixelIndex += 1;
+	for letters in message:
+	    for bit in getLetterAsByte(letters):
 
-    # pular o nono byte    
-    pixelIndex +=1;
+		readByte = getLastBit(image[pixelIndex]) + readByte
+
+		if bit == 3:
+		    print("Message found in file:", decodedWord)
+		    break
+
+		readByte = getLastBit(image[pixelIndex]) + readByte
+
+		if(decodedWord == message):
+		    print("Message found in file:", decodedWord)
+		    break
+		pixelIndex += 1
+
+	    # pular o nono byte
+	    pixelIndex += 1
+
+
+if "-d" in sys.argv:
+	decipher()
+else:
+	cipher()
